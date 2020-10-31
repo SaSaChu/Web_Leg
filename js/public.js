3823576f494d86b41a2046f176fbba6666079087
TeresaChu <tsgo5888@gmail.com>
2020-11-01 03:28:38 +0800

    首頁修改完成大部分，剩下小調整

--- js/public.js
+++ js/public.js
@@ -3,5 +3,39 @@
 $(function() {
 
   $(".i_img_new, .i_img_s").imgLiquid ();
+
+  $('#r_menu_boxs').each (function () {
+    var $that = $(this);
+    $that.find ('span').click (function () {
+      $(this).toggleClass ('s');
+    });
+  });
+
+  $('.smenu').click(function() {
+    $('.smenu_boxs').addClass('smenu_open');
+  });
+
+  $('.icon-menu').click(function() {
+    $('.main').addClass('nav_hover');
+    $('#r_menu_boxs').addClass('nav_open');
+    $('#menu_back ').addClass('b_open');
+    scrollLock();
+  });
+
+
+  $('#menu_back').click(function() {
+    $('.main').removeClass('nav_hover');
+    $('#r_menu_boxs').removeClass('nav_open');
+    $('#menu_back').removeClass('b_open');
+    scrollUnlock();
+  });
+
+  $('.icon-close').click(function() {
+    $('.main').removeClass('nav_hover');
+    $('#r_menu_boxs').removeClass('nav_open');
+    $('#menu_back').removeClass('b_open');
+    scrollUnlock();
+  });
+
   
 });
