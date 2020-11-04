70cfe7ee381fbcb4731a8251424d0a97fe22acac
SaSaChu <teresa7397@gmail.com>
2020-11-04 16:38:20 +0800

    fq完成

--- js/public.js
+++ js/public.js
@@ -2,8 +2,10 @@
  
 $(function() {
 
+  // 圖片縮放
   $(".i_img_new, .i_img_s, .p_img_new, .p_banner_boxs460").imgLiquid ();
 
+  // 手機選單
   $('#r_menu_boxs').each (function () {
     var $that = $(this);
     $that.find ('span').click (function () {
@@ -38,4 +40,28 @@ $(function() {
   });
 
   
+  // 手風琴
+  var oneOpen = true;
+
+  // 這裡就看你怎麼命名就改就好
+  $('#accordion .p_page').each (function () {
+    // 內容不用看
+    $(this).data ('height', $(this).outerHeight ());
+    $(this).get (0).$span = $(this).find ('>span');
+
+    
+    $(this).get (0).run = function () {
+      if ($(this).hasClass ('show')) $(this).css ({ height: $(this).data ('height') }) && $(this).hasClass ('show') && oneOpen && $(this).siblings ().removeClass ('show').each (function () { if ($(this).get (0).$span) $(this).css ({ height: $(this).get (0).$span.outerHeight (true) }); });
+      else $(this).css ({ height: $(this).get (0).$span.outerHeight (true) });
+    }.bind ($(this));
+
+    $(this).get (0).$span.click (function () {
+      if ($(this).hasClass ('show')) $(this).removeClass ('show');
+      else $(this).addClass ('show');
+      $(this).get (0).run ();
+    }.bind ($(this)));
+    
+    $(this).get (0).run ();
+  });
+
 });
